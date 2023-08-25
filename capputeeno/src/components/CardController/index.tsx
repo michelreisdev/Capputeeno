74d235d70148171dbe4f61ea7a7d91026f201703
Michelreisdev <michelreisdev@gmail.com>
2023-08-25 17:05:22 -0300

    fix: corrige hook LocalStorage

--- capputeeno/src/components/CardController/index.tsx
+++ capputeeno/src/components/CardController/index.tsx
@@ -1,14 +1,30 @@
 import { useLocalStorage } from '@/hooks/useLocalStorage'
 import BagBar from '../BagBar'
 import CarCount from '../CarCount'
+import { styled } from 'styled-components'
+
+const CartCount = styled.span`
+  width: 17px;
+  height: 17px;
+  border-radius: 100%;
+  padding: 0 5px;
+  font-size: 10px;
+  background-color: var(--delete-color);
+  color: white;
+  margin-left: -10px;
+`
+
+const Container = styled.div`
+  position: relative;
+`
 
 export default function CardController() {
-  const { value } = useLocalStorage('card-items')
+  const { value } = useLocalStorage('cart-items', [])
 
   return (
-    <div>
+    <Container>
       <BagBar />
-      {value.length && <CarCount />}
-    </div>
+      {value.length && <CartCount>{value.length}</CartCount>}
+    </Container>
   )
 }
--- capputeeno/src/hooks/useLocalStorage.tsx
+++ capputeeno/src/hooks/useLocalStorage.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-export function useLocalStorage<T>(item: string) {
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(item) ?? 'null'),
-  )
+export function useLocalStorage<T>(item: string, initialValue: T) {
+  const [value, setValue] = useState<T>(initialValue)
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+    const value = localStorage.getItem(item)
+    if (value) setValue(JSON.parse(value))
+  }, [window])
 
   const updateLocalStorage = (newValue: T) => {
     setValue(newValue)
