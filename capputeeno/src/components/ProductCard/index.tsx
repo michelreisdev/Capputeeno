6031669f0445f16d7ec3d1a315bc871ccc95816b
Michelreisdev <michelreisdev@gmail.com>
2023-08-28 17:30:11 -0300

    feat: card product

---
+++ capputeeno/src/components/ProductCard/index.tsx
@@ -0,0 +1,21 @@
+import { formatPrice } from '@/utils/format-price'
+import { Card } from './styles'
+interface ProductCardProps {
+  image: string
+  title: string
+  price: number
+}
+
+export default function ProductCard(props: ProductCardProps) {
+  const price = formatPrice(props.price)
+  return (
+    <Card>
+      <img src={props.image} />
+      <div>
+        <h3>{props.title}</h3>
+        <div></div>
+        <p>{price}</p>
+      </div>
+    </Card>
+  )
+}
--- capputeeno/src/components/ProductsList/index.tsx
+++ capputeeno/src/components/ProductsList/index.tsx
@@ -1,7 +1,28 @@
 import { useProducts } from '@/hooks/useProducts'
+import ProductCard from '../ProductCard'
+import styled from 'styled-components'
+
+const ListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 256px);
+  grid-gap: 32px;
+  max-width: 100%;
+
+  margin-top: 32px;
+`
 
 export default function ProductList() {
   const { data } = useProducts()
-  console.log(data)
-  return <></>
+  return (
+    <ListContainer>
+      {data?.map((product) => (
+        <ProductCard
+          key={product.id}
+          title={product.name}
+          price={product.price_in_cents}
+          image={product.image_url}
+        />
+      ))}
+    </ListContainer>
+  )
 }
